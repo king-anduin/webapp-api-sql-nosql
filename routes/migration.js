8745269c621537b766ae2693b58663b3792a0284
Peter Stueben <peter.stueben@jodocus.io>
2021-10-19 16:53:52 +0200

    nosql edited

--- routes/migration.js
+++ routes/migration.js
@@ -126,3 +126,46 @@ async function getCollectionWaypoint() {
   }
 }
 getCollectionWaypoint();
+
+//------------------------- Change old id to new _id ----------------------//
+async function updateIds() {
+  var get_idClient = await client.find({});
+  var getClientCount = await client.count();
+  for (i = 1; i <= getClientCount; i++) {
+    await ride.updateMany({ client_id: i }, { client_id: get_idClient[i - 1]._id });
+    console.log(i, get_idClient[i - 1]._id);
+  }
+  console.log(getClientCount);
+  var get_idDriver = await driver.find({});
+  var getDriverCount = await driver.count();
+  for (i = 1; i <= getDriverCount; i++) {
+    await ride.updateMany({ driver_id: i }, { driver_id: get_idDriver[i - 1]._id });
+    console.log(i, get_idDriver[i - 1]._id);
+  }
+  console.log(getDriverCount);
+  var get_idRide = await ride.find({});
+  var getRideCount = await ride.count();
+  for (i = 1; i <= getRideCount; i++) {
+    await waypoint.updateMany({ ride_id: i }, { ride_id: get_idRide[i - 1]._id });
+    console.log(i, get_idRide[i - 1]._id);
+  }
+  console.log(getRideCount);
+}
+
+//------------------------- Check migration ----------------------//
+async function checkIdMigration() {
+  zero = 0;
+  var getClientCount = await client.count();
+  var getDriverCount = await driver.count();
+  var getRideCount = await ride.count();
+  var getWaypointCount = await waypoint.count();
+  if (
+    getClientCount === zero &&
+    getDriverCount === zero &&
+    getRideCount === zero &&
+    getWaypointCount === zero
+  ) {
+    updateIds();
+  }
+}
+checkIdMigration();
